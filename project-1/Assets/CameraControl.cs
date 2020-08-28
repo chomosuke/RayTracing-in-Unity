a5c27a738cd9884e9f176536ee4344992b9dd824
JasperNg13 <jasperkmng@gmail.com>
2020-08-28 21:55:03 +1000

    Camera Control
    Camera Control first iteration

---
+++ project-1/Assets/CameraControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+
+    public float sensitivity = 2.0f;
+
+    public Quaternion camRotation;
+
+    // Update is called once per frame
+    void Update()
+    {  
+
+        camRotation.x -= sensitivity * Input.GetAxis("Mouse Y"); // Look left/right
+        camRotation.y += sensitivity * Input.GetAxis("Mouse X"); // Look up/down
+
+        transform.localRotation = Quaternion.Euler(camRotation.x, camRotation.y, camRotation.z);
+
+    }
+
+}
---
+++ project-1/Assets/CameraMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    
+    public float cameraSpeed = 5.0f;
+
+    public CameraControl cameraDirection;
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+        if (Input.GetKey(KeyCode.D)) {
+            this.transform.localPosition += cameraDirection.transform.right * cameraSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            this.transform.localPosition += cameraDirection.transform.right * -1 * cameraSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.W)) {
+            this.transform.localPosition += cameraDirection.transform.forward * cameraSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            this.transform.localPosition += cameraDirection.transform.forward * -1 * cameraSpeed * Time.deltaTime;
+        }
+
+    }
+}
