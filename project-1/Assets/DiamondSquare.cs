c10310f5beb4f7df5c8a8407c4cbe0df81163f42
Juwey <juweytan@gmail.com>
2020-08-28 21:41:48 +1000

    Diamond Square Algorithm Bug Fixed

--- project-1/Assets/DiamondSquare.cs
+++ project-1/Assets/DiamondSquare.cs
@@ -18,7 +18,7 @@ public class DiamondSquare : MonoBehaviour
         landScapeMesh = this.gameObject.AddComponent<MeshFilter>();
         landScapeMesh.mesh = this.CreateLandScapeMesh(iterations);
         MeshRenderer renderer = this.gameObject.AddComponent<MeshRenderer>();
-        renderer.material.shader = Shader.Find("Unlit/VertexColorShader");
+        renderer.material.shader = Shader.Find("Unlit/CubeShader");
     }
 
     // Update is called once per frame
@@ -34,18 +34,19 @@ public class DiamondSquare : MonoBehaviour
         m.name = "Landscape";
 
         m.vertices = GenerateVectors(iterations);
-        m.colors =  new Color[m.vertices.Length];
-        for (int i = 0; i < m.colors.Length; i++)
+        Color[] colors =  new Color[m.vertices.Length];
+        for (int i = 0; i < colors.Length; i++)
         {
-            m.colors[i] = Color.blue;
+            colors[i] = Color.blue;
         }
+        m.colors = colors;
         m.SetTriangles(GenerateTriangles(m.vertices),0);
         return m;
     }
 
     private Vector3[] GenerateVectors(int iterations){
         float[][] grid = GenerateHeightMap(iterations);
-        Vector3[] vectors = new Vector3[grid.Length^2];
+        Vector3[] vectors = new Vector3[grid.Length * grid.Length];
         for (int x = 0; x < grid.Length; x++)
         {
             for (int y = 0; y < grid.Length; y++)
@@ -80,21 +81,30 @@ public class DiamondSquare : MonoBehaviour
         {
             // Diamond Step
             // Traversing squares
-            for (int i = 0; i < length; i+=currentLength)
+            for (int i = 0; i < length-1; i+=currentLength)
             {
-                for (int j = 0; j < length; j+=currentLength)
+                for (int j = 0; j < length-1; j+=currentLength)
                 {
+                    float slot = (grid[i][j] + grid[i+currentLength][j] 
+                        + grid[i][j+currentLength] + grid[i+currentLength][j+currentLength]) / 4;
+                        
                     grid[i+(currentLength/2)][j+(currentLength/2)]
-                        = (grid[i][j] + grid[i+currentLength][j] 
-                        + grid[i][j+currentLength] + grid[i+currentLength][j+currentLength]) / 4 
-                        + RandomRange(randomness/2, -randomness/2);
+                        = slot + RandomRange(randomness/2, -randomness/2);
                 }
             }
 
+            Boolean isEvenLine = true;
             // Square Step
-            for (int i = currentLength/2; i < length; i+=currentLength)
+            for (int i = 0; i < length; i+=(currentLength/2))
             {
-                for (int j = currentLength/2; j < length; j+=currentLength)
+                int j;
+                if(isEvenLine){
+                    j = currentLength/2;
+                }
+                else{
+                    j = 0;
+                }
+                for (; j < length; j+=currentLength)
                 {
                     ArrayList heights = new ArrayList();
                     heights.Add(new int[2]{i+currentLength/2, j});
@@ -117,6 +127,7 @@ public class DiamondSquare : MonoBehaviour
                     }
                     grid[i][j] = sum / heights.Count + RandomRange(randomness/2, -randomness/2);
                 }
+                isEvenLine = !isEvenLine;
             }
 
             currentLength /= 2;
@@ -127,14 +138,14 @@ public class DiamondSquare : MonoBehaviour
 
     private int[] GenerateTriangles(Vector3[] vectorsPreTriangle){
         int squaresOnEdge = (((int)Math.Sqrt(vectorsPreTriangle.Length)-1));
-        int[] triangles = new int[(squaresOnEdge^2)*6];
+        int[] triangles = new int[(squaresOnEdge * squaresOnEdge)*6];
         int x = 0;
         int y = 0;
         for (int i = 0; i < triangles.Length; i+=6)
         {
             triangles[i] = y*(squaresOnEdge+1)+x;
-            triangles[i+1] = y*(squaresOnEdge+1)+x+1;
-            triangles[i+2] = (y+1)*(squaresOnEdge+1)+x;
+            triangles[i+1] = (y+1)*(squaresOnEdge+1)+x;
+            triangles[i+2] = y*(squaresOnEdge+1)+x+1;
             triangles[i+3] = y*(squaresOnEdge+1)+x+1;
             triangles[i+4] = (y+1)*(squaresOnEdge+1)+x;
             triangles[i+5] = (y+1)*(squaresOnEdge+1)+x+1;
