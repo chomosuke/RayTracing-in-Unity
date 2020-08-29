b9f79f1a8d977a5704b2ffb4fc987d257a81ccc4
chomosuke <38484873+chomosuke@users.noreply.github.com>
2020-08-29 23:22:54 +1200

    removed camera control in camera movement

--- project-1/Assets/CameraMovement.cs
+++ project-1/Assets/CameraMovement.cs
@@ -7,23 +7,21 @@ public class CameraMovement : MonoBehaviour
     
     public float cameraSpeed = 5.0f;
 
-    public CameraControl cameraDirection;
-
     // Update is called once per frame
     void Update()
     {
     
         if (Input.GetKey(KeyCode.D)) {
-            this.transform.localPosition += cameraDirection.transform.right * cameraSpeed * Time.deltaTime;
+            this.transform.localPosition += gameObject.transform.right * cameraSpeed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.A)) {
-            this.transform.localPosition += cameraDirection.transform.right * -1 * cameraSpeed * Time.deltaTime;
+            this.transform.localPosition += gameObject.transform.right * -1 * cameraSpeed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.W)) {
-            this.transform.localPosition += cameraDirection.transform.forward * cameraSpeed * Time.deltaTime;
+            this.transform.localPosition += gameObject.transform.forward * cameraSpeed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.S)) {
-            this.transform.localPosition += cameraDirection.transform.forward * -1 * cameraSpeed * Time.deltaTime;
+            this.transform.localPosition += gameObject.transform.forward * -1 * cameraSpeed * Time.deltaTime;
         }
 
     }
