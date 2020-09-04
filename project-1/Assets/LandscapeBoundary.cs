2854420510e24055204bf87a38e33df828ecc901
jasrobo <jasmine.bond@outlook.com>
2020-09-04 15:41:12 +1000

    Adjusted size & location of boundary box
    Reduced the thickness of each box collider, and the landscape is now centred in the boundary box

--- project-1/Assets/LandscapeBoundary.cs
+++ project-1/Assets/LandscapeBoundary.cs
@@ -10,7 +10,11 @@ public class LandscapeBoundary : MonoBehaviour
     // Assign edge to boundary in editor
     public Side edge;
 
+    // Determines space between boundary and landscape
     public float margin = 1.0f;
+
+    // Thickness of boundary
+    private float thickness = 0.1f;
 
     private bool boundarySet = false;
     
@@ -43,32 +47,32 @@ public class LandscapeBoundary : MonoBehaviour
         
         // Setting the size of box collider for the edge
         if(edge == Side.Top || edge == Side.Bottom) {
-            boundary.size = new Vector3(unitSide, margin, unitSide);
+            boundary.size = new Vector3(unitSide, thickness, unitSide);
         } else if (edge == Side.Front || edge == Side.Back) {
-            boundary.size = new Vector3(unitSide, unitHeight*4.0f, margin);
+            boundary.size = new Vector3(unitSide, unitHeight*2.0f, thickness);
         } else {
-            boundary.size = new Vector3(margin, unitHeight*4.0f, unitSide);
+            boundary.size = new Vector3(thickness, unitHeight*2.0f, unitSide);
         }
 
-        // Sets the position of the box collider (size: total height = 4 * unitHeight, side = unitSide)
+        // Sets the position of the box collider (size: total height = 2 * unitHeight, side = unitSide)
         switch(edge) {
             case Side.Top:
-               this.gameObject.transform.position = new Vector3(unitSide/2, unitHeight*3.0f, unitSide/2);
+               this.gameObject.transform.localPosition = new Vector3(unitSide/2 - margin/2, unitHeight*2.0f, unitSide/2 - margin/2);
                 break;
             case Side.Bottom:
-                this.gameObject.transform.position = new Vector3(unitSide/2, -unitHeight, unitSide/2);
+                this.gameObject.transform.localPosition = new Vector3(unitSide/2 - margin/2, 0.0f, unitSide/2 - margin/2);
                 break;
             case Side.Left:
-                this.gameObject.transform.position = new Vector3(0.0f, unitHeight, unitSide/2);
+                this.gameObject.transform.localPosition = new Vector3(-margin/2, unitHeight, unitSide/2 - margin/2);
                 break;
             case Side.Right:
-                this.gameObject.transform.position = new Vector3(unitSide, unitHeight, unitSide/2);
+                this.gameObject.transform.localPosition = new Vector3(unitSide - margin/2, unitHeight, unitSide/2 - margin/2);
                 break;
             case Side.Front:
-                this.gameObject.transform.position = new Vector3(unitSide/2, unitHeight, unitSide);
+                this.gameObject.transform.localPosition = new Vector3(unitSide/2 - margin/2, unitHeight, unitSide - margin/2);
                 break;
             case Side.Back:
-                this.gameObject.transform.position = new Vector3(unitSide/2, unitHeight, 0.0f);
+                this.gameObject.transform.localPosition = new Vector3(unitSide/2 - margin/2, unitHeight, -margin/2);
                 break;
         }
 
