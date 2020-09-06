f07c7a9269e70b611db22524879b7ef3d5d9a5b5
chomosuke <38484873+chomosuke@users.noreply.github.com>
2020-09-06 20:42:07 +1200

    rotating sun

---
+++ project-1/Assets/Rotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, Time.deltaTime * speed), Space.World);
+    }
+}
