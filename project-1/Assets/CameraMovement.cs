5a0e33b6f22e3156b730576f7009c729a57bc7a1
chomosuke <38484873+chomosuke@users.noreply.github.com>
2020-09-14 17:29:22 +1200

    camera boundary perfect now (at least we hope)

--- project-1/Assets/CameraMovement.cs
+++ project-1/Assets/CameraMovement.cs
@@ -20,7 +20,6 @@ public class CameraMovement : MonoBehaviour
     {
         Vector3 previousPosition = this.gameObject.transform.position;
 
-
         GameObject landscape = GameObject.Find("Landscape");
 
         if(!positionSet && landscape.GetComponent<MeshFilter>() != null) {
@@ -36,11 +35,11 @@ public class CameraMovement : MonoBehaviour
             this.gameObject.transform.position = landscapeVertices[landscapeVertices.Length/2];
             this.gameObject.transform.position += Vector3.up * maxY;
         }
-    
+
         if (Input.GetKeyDown(KeyCode.Space)){
             positionSet = false;
         }
-    
+
         if (Input.GetKey(KeyCode.D)) {
             this.transform.localPosition += gameObject.transform.right * cameraSpeed * Time.deltaTime;
         }
@@ -61,10 +60,11 @@ public class CameraMovement : MonoBehaviour
 
     bool landscape_collision (Vector3 newPosition) {
         float waveHeight = GameObject.Find("Water").GetComponent<WaterReflection>().waveHeight;
+        float threshold = 0.01f;
 
         // if camera is at the edge of the map
         if(newPosition.x > gridSize || newPosition.x < 0 || newPosition.z > gridSize || newPosition.z < 0 
-             || newPosition.y > maxY + gridSize || newPosition.y <= (minY + waveHeight)) {
+             || newPosition.y <= (minY + waveHeight + threshold)) {
             return true;
         } else {
             // number of vectors in one row of grid
@@ -81,13 +81,13 @@ public class CameraMovement : MonoBehaviour
             // gets group of 4 vectors around closest vector
             Vector3[] closestPositions = {
                 landscapeVertices[closestZ * gridLength + closestX], // closest point
-                landscapeVertices [(closestZ * gridLength + closestX) + 1], 
+                landscapeVertices [closestZ * gridLength + (closestX + 1)], 
                 landscapeVertices[(closestZ + 1) * gridLength + closestX], 
-                landscapeVertices[((closestZ + 1) * gridLength + closestZ) + 1]
+                landscapeVertices[(closestZ + 1) * gridLength + (closestX + 1)]
             };
 
             foreach(Vector3 position in closestPositions) {
-                if(newPosition.y <= (position.y)) {
+                if(newPosition.y <= (position.y + threshold)) {
                     return true;
                 }
             }
